5bf3bed39832e6235287e7366ae2694cbf8d3d88
irsalaska <162007936+irsalaska@users.noreply.github.com>
2024-05-22 20:39:23 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Information</h1>
+      </header>
+      <main>
+        <section>
+          <h2>About Me</h2>
+          <p>My name is I.Dzulfiqor Askarulhaq</p>
+          <p>I am a college student at Padjajaran University.</p>
+          <p>I am studying Business Digital.</p>
+        </section>
+        <section>
+          <h2>Contact Information</h2>
+          <p>Email: irsalistafia@gmail.com</p>
+          <p>Phone: 082112871580</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
